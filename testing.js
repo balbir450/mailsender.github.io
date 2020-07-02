fc6804f41004abc7bdc48bdb2f9f469702c5a107
balbir450 <65977766+balbir450@users.noreply.github.com>
2020-07-02 05:21:35 -0400

    Update testing.js

--- testing.js
+++ testing.js
@@ -13,16 +13,12 @@ var smtpTransport = nodemailer.createTransport({
         pass: "5starchoco"
     }
 });
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-};
-app.configure(function() {
-    app.use(allowCrossDomain);
-    //some other code
-}); 
+
+ app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
 /*------------------SMTP Over-----------------------------*/
 
 /*------------------Routing Started ------------------------*/
